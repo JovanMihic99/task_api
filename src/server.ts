b15b68a7486bbb1667f392a6f4568af0ab0c921c
JovanMihic99 <jovanmihic99@gmail.com>
2024-12-07 19:08:33 +0100

    feat: add swagger docs

--- src/server.ts
+++ src/server.ts
@@ -3,12 +3,13 @@ import dotnev from "dotenv"
 import taskRouter from "./routes/task-router";
 import userRouter from "./routes/user-router";
 import errorHandler from './middleware/error-handler'
-
+import setupSwagger from "../docs/swagger";
 dotnev.config();
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(express.json());
+setupSwagger(app);
 
 // Routing 
 const router = express.Router();
@@ -21,6 +22,9 @@ app.use(errorHandler);
 
 app.listen(port, () => {
     console.log(`Task API listening on port ${port}...`);
+    console.log(
+        `API documentation is available at http://localhost:${port}/api-docs`
+      );
 });
 
 export default app;
