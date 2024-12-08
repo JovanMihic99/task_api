6d9269f86d099500632010b9a7be04bc748dc706
JovanMihic99 <jovanmihic99@gmail.com>
2024-12-08 17:03:16 +0100

    fix: seed the database if there are no entries when starting docker containers

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,13 @@
 #!/bin/sh
-# Run migrations
+
+# Run migrations (make sure the database schema is up-to-date)
+echo "Running migrations..."
 npm run migrate
 
-# Seed the database
+# Seed the database (only run after migrations are applied)
+echo "Seeding the database..."
 npx ts-node prisma/seed.ts
 
 # Start the development server
+echo "Starting the server..."
 npm start
